33834af6e82d781d1fcb791b4d4353b8b86c2ace
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-20 13:04:51 +0100

    feat: Added home.blade html code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,24 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+@extends('layouts.app')
+
+@section('content')
+    <div class="journal">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Date</th>
+                    <th scope="col">Entry</th>
+                    <th scope="col">Emotion</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($journals as $journal)
+                    <tr>
+                        <td>{{$journal->created_at}}</td>
+                        <td>{{$journal->entry}}</td>
+                        <td><img src="{{asset($journal->emotion)}}" class="emotionImg" alt="emotion"></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
