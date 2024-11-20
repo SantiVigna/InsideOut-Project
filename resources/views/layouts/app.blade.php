1beae9c4cb28c782de557e4077e2d1de1e0720e8
rebecabernal <rebecabernalmesa@gmail.com>
2024-11-20 12:13:25 +0100

    refactor: layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,25 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Mi Aplicación')</title>
-    <!-- Agrega aqui tus estilos -->
+    <title>@yield('title', 'Inside Out')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <div id="app">
-        <!-- Encabezado común -->
-        <x-header />
-        
-        <!-- Contenido principal -->
+        <x-header />  
+
         <main>
         @yield('content')
         </main>
 
-        <!-- Pie de página común -->
         <x-footer />
     </div>
-
-    <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
