9b2d5f3b4754e3491bfdbe0cd85ce0dae2cda5fb
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-11-20 11:35:40 +0100

    fix: Fixed typos

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,5 @@ use App\Http\Controllers\Api\JournalController;
 Route::get('/journals', [JournalController::class, 'index'])->name('apihome');
 Route::post('/journals', [JournalController::class, 'store'])->name('apistore');
 Route::get('/journals/{id}', [JournalController::class, 'show'])->name('apishow');
-ROute::put('/journals/{id}', [JournalController::class, 'update'])->name('apiupdate');
+Route::put('/journals/{id}', [JournalController::class, 'update'])->name('apiupdate');
 Route::delete('/journals/{id}', [JournalController::class, 'destroy'])->name('apidestroy');
