479b1da91988bcd4fc1dd698b08b7cf28d0ca89c
talenaa <talenabarbosa7@gmail.com>
2024-11-21 08:50:56 +0100

    refactor: test_example and ListOfProductsCanBeRetrieved are working

--- tests/Feature/JournalTest.php
+++ tests/Feature/JournalTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Journal;
 
 class JournalTest extends TestCase
 {
@@ -12,13 +13,22 @@ class JournalTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_ListOfEntryCanBeRead(){
+    public function test_example(): void
+    {
+        $response = $this->get('/');
 
+        $response -> assertStatus(200);
+    }
+
+    public function test_ListOfProductsCanBeRetrieved()
+    {
         $this->withoutExceptionHandling();
+
+        Journal::all();
 
         $response = $this->get('/');
 
         $response->assertStatus(200)
-                 ->assertViewIs('home');
+                ->assertViewIs('home');
     }
 }
